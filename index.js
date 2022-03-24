fa6e538f4490276d6f688c42d9982daf636014c9
Daniel Becker <daniel.becker000@gmail.com>
2022-03-23 18:17:18 -0700

    commit tree 1c72346abb4e8a80aa27486f7a00b4a3eab6e99e

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+// goal: event listeners that move the dodger to the left/right
+// reactive to the left/right arrow keys 
+
+// steps: define the dodger
+// write two functions for moveDodgerLeft and moveDodgerRight
+// call those functions in two event listeners for 
+//  -- "ArrowKey" left / right
+
+dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft ();
+    } 
+});
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight ();
+    }
+});
+
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left>0) {
+        dodger.style.left = `${left-1}px`
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers, 10);
+    if (left<360) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+
